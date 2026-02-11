3af93be6ea8b4c11ed701fcdbddb31b7f1b50f84
J Patrick Connor <patrick_connor@dfci.harvard.edu>
2026-02-11 17:18:28 -0500

    added bash scripts folder

---
+++ bash_scripts/.ipynb_checkpoints/full_model_run_wo_stage-checkpoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+#SBATCH --job-name=full_model_wo_stage_run
+
+#SBATCH --partition=normal 
+
+#SBATCH --ntasks=1 
+
+#SBATCH --cpus-per-task=16
+
+#SBATCH --mem=32G
+
+#SBATCH --output=output/full_model_wo_stage.out 
+
+#SBATCH --error=error/full_model_wo_stage.err
+
+source /PHShome/jpc91/.bashrc
+module load miniforge3
+conda activate clinical_notes_project
+
+python /data/gusev/USERS/jpconnor/clinical_text_project/code/python_scripts/model_training/run_unified_model_wo_stage.py
+
+conda deactivate
---
+++ bash_scripts/feature_specific_risk.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+#SBATCH --job-name=full_cohort_run
+
+#SBATCH --partition=normal 
+
+#SBATCH --ntasks=1 
+
+#SBATCH --cpus-per-task=16
+
+#SBATCH --mem=32G
+
+#SBATCH --output=output/feature_specific_risk.out 
+
+#SBATCH --error=error/feature_specific_risk.err
+
+source /PHShome/jpc91/.bashrc
+module load miniforge3
+conda activate clinical_notes_project
+
+python /data/gusev/USERS/jpconnor/clinical_text_project/code/python_scripts/model_evaluation/feature_ICD10_level_3_risk_scores.py
+
+conda deactivate
